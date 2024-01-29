f1c94ad5f921fd01416e0c76ffd534e817f29481
tatiana hoyos <tatiana98@gmail.com>
2024-01-29 13:58:49 -0500

    tratando de agregar el menu modificado a compras

--- Frontend/WEB/js/menuLateral.js
+++ Frontend/WEB/js/menuLateral.js
@@ -1,6 +1,28 @@
-const sideNav = document.querySelector('.side-nav');
-const sideNavToggle = document.querySelector('.side-nav-toggle');
+// En tu archivo JavaScript
+fetch('menu.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('menu-container').innerHTML = data;
+    initMenu();
+  })
+  .catch(error => console.error('Error al cargar el menú:', error));
 
-sideNavToggle.addEventListener('click', () => {
-  sideNav.classList.toggle('open');
-});
+function initMenu() {
+  // Luego de cargar el menú, puedes realizar inicializaciones específicas del menú aquí
+  // Por ejemplo, puedes inicializar eventos, estilos, etc.
+
+  const sideNav = document.querySelector('.side-nav');
+  const sideNavToggle = document.querySelector('.side-nav-toggle');
+  const closeMenuBtn = document.querySelector('.close-menu-btn');
+
+  sideNavToggle.addEventListener('click', () => {
+    sideNav.classList.toggle('open');
+    sideNavToggle.classList.toggle('hidden');
+  });
+
+  closeMenuBtn.addEventListener('click', () => {
+    sideNav.classList.remove('open');
+    sideNavToggle.classList.remove('hidden');
+  });
+  console.log('initMenu ejecutado');
+}
