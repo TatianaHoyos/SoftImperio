72c27755adafd401e5ae51282e310037d5914d31
tatiana Hoyos <tatianavertel98@gmail.com>
2024-02-15 17:56:38 -0500

    validaciones en producto

--- Frontend/WEB/js/validaciones.js
+++ Frontend/WEB/js/validaciones.js
@@ -1,24 +1,53 @@
-function validarNumero(e){
-    tecla = (document.all) ? e.keyCode : e.which;
-    if (tecla==8) return true;
-    patron =/[0-9]/;
-    te = String.fromCharCode(tecla);
-    return patron.test(te)
-}
-function validarNumeroUC(e) {
-    tecla = (document.all) ? e.keyCode : e.which;
-    if (tecla == 8) return true;
-    patron = /[0-9]/;
-    te = String.fromCharCode(tecla);
+// function validarNumero(e){
+//     tecla = (document.all) ? e.keyCode : e.which;
+//     if (tecla==8) return true;
+//     patron =/[0-9]/;
+//     te = String.fromCharCode(tecla);
+//     return patron.test(te)
+// }
+
+// function validarNumeroUC(e) {
+//     tecla = (document.all) ? e.keyCode : e.which;
+//     if (tecla == 8) return true;
+//     patron = /[0-9]/;
+//     te = String.fromCharCode(tecla);
     
-    // Verifica si la longitud actual del campo es menor o igual a 14 (ya que se permiten 15 caracteres)
-    if (e.target.value.length <= 14) {
-      return patron.test(te);
-    } else {
-      // Si ya hay 15 caracteres, no se permite ingresar más
-      return false;
+//     // Verifica si la longitud actual del campo es menor o igual a 14 (ya que se permiten 15 caracteres)
+//     if (e.target.value.length <= 14) {
+//       return patron.test(te);
+//     } else {
+//       // Si ya hay 15 caracteres, no se permite ingresar más
+//       return false;
+//     }
+//   }
+
+function soloNumeros(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key);
+    numeros = "0123456789";
+    especiales = "8-37-38-46";
+
+    tecla_especial = false
+    for (var i in especiales) {
+        if (key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
     }
-  }
+
+    if (numeros.indexOf(tecla) === -1 && !tecla_especial) {
+        mostrarMensaje('Solo se permiten números', 'red', '#mensajeNumeros');
+        return false;
+    } else {
+        // Limpiar el mensaje si la tecla ingresada es válida
+        $('#mensajeNumeros').text('');
+     
+        $('#mensajeNumerosPrecio').text('');
+     
+    }
+}
+
+
 
   function sololetrasnombre(e) {
     var key = e.keyCode || e.which;
@@ -29,44 +58,73 @@ function validarNumeroUC(e) {
     if (letras.indexOf(tecla) == -1 || e.target.value.length >= 30) {
       return false;
     }
-  }
-
-
-  function sololetras(e){
-    key= e.keyCode || e.which;
-    tecla = String.fromCharCode(key).toLowerCase();
-    letras = "àèìòùabcdefghijklmnñopqrstuvwxyz";
-    tecla_especial = false
-    for(var i in especiales){
-        if(key == especiales[i]){
-            tecla_especial = true;
-            break;
-        }
-    }
-
-    if(letras.indexOf(tecla)==-1 && !tecla_especial){
-        return false;
-    }
 }
 
-function sololetras(e){
-    key= e.keyCode || e.which;
+
+// function sololetras(e){
+//     key= e.keyCode || e.which;
+//     tecla = String.fromCharCode(key).toLowerCase();
+//     letras = "àèìòùabcdefghijklmnñopqrstuvwxyz";
+//     tecla_especial = false
+//     for(var i in especiales){
+//         if(key == especiales[i]){
+//             tecla_especial = true;
+//             break;
+//         }
+//     }
+
+//     if(letras.indexOf(tecla)==-1 && !tecla_especial){
+//         return false;
+//     }
+
+// }
+
+// function sololetras(e){
+//     key= e.keyCode || e.which;
+//     tecla = String.fromCharCode(key).toLowerCase();
+//     letras = "àèìòùabcdefghijklmnñopqrstuvwxyz";
+//     especiales = "8-37-38-46-164";
+
+//     tecla_especial = false
+//     for(var i in especiales){
+//         if(key == especiales[i]){
+//             tecla_especial = true;
+//             break;
+//         }
+//     }
+
+//     if(letras.indexOf(tecla)==-1 && !tecla_especial){
+//         return false;
+//     }
+// }
+function sololetras(e) {
+    key = e.keyCode || e.which;
     tecla = String.fromCharCode(key).toLowerCase();
     letras = "àèìòùabcdefghijklmnñopqrstuvwxyz";
     especiales = "8-37-38-46-164";
 
     tecla_especial = false
-    for(var i in especiales){
-        if(key == especiales[i]){
+    for (var i in especiales) {
+        if (key == especiales[i]) {
             tecla_especial = true;
             break;
         }
     }
 
-    if(letras.indexOf(tecla)==-1 && !tecla_especial){
+    if (letras.indexOf(tecla) === -1 && !tecla_especial) {
+        mostrarMensaje('Solo se permiten letras', 'red', '#mensajeLetras');
         return false;
+    } else {
+        // Limpiar el mensaje si la tecla ingresada es válida
+        $('#mensajeLetras').text('');
     }
 }
+
+function mostrarMensaje(mensaje, color, elemento) {
+    $(elemento).text(mensaje);
+    $(elemento).css('color', color);
+}
+
 
 function contrasena(e){
   key1= e.keyCode || e.which;
