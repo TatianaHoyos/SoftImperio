dbfab7193a4eca5082eaa1bc960fe874bf2cab17
tatiana Hoyos <tatianavertel98@gmail.com>
2024-03-16 14:25:05 -0500

    se crea archivo docker para api c#

--- Frontend/WEB/js/validaciones.js
+++ Frontend/WEB/js/validaciones.js
@@ -119,32 +119,16 @@ function sololetrasnombre(e) {
         return false;
     }
 }
-
-function sololetras(e) {
-    key = e.keyCode || e.which;
-    tecla = String.fromCharCode(key).toLowerCase();
-    letras = "àèìòùabcdefghijklmnñopqrstuvwxyz";
-    especiales = "8-37-38-46-164";
-
-    tecla_especial = false
-    for (var i in especiales) {
-        if (key == especiales[i]) {
-            tecla_especial = true;
-            break;
-        }
-    }
-
-    if (letras.indexOf(tecla) === -1 && !tecla_especial) {
-        mostrarMensaje('Solo se permiten letras', 'red', '#mensajeLetras');
-        return false;
-    } else {
-        // Limpiar el mensaje si la tecla ingresada es válida
-        $('#mensajeLetras').text('');
-    }
-
+function validarNombre() {
    
-    
+        var charCode = event.which ? event.which : event.keyCode;
+        if ((charCode < 65 || charCode > 90) && (charCode < 97 || charCode > 122)) {
+            event.preventDefault();
+            return false;
+        }
+        return true;
 }
+
 
 
 function mostrarMensaje(mensaje, color, elemento) {
