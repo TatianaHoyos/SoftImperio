18e6803f84334bfa251d7f3c16005185ff202cec
TatianaHoyos <113327153+TatianaHoyos@users.noreply.github.com>
2024-03-20 17:48:38 -0500

    Update ServiceApplicationTests.java

--- Backend/api_seguridad/src/test/java/com/imperio/service/ServiceApplicationTests.java
+++ Backend/api_seguridad/src/test/java/com/imperio/service/ServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.imperio.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RunWith(MockitoJUnitRunner.class)
 class ServiceApplicationTests {
 
 	@Test
