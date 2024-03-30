1698caa441fdb3dd757967a30f9c99e2caae3138
Daniel <edbravo@misena.edu.co>
2024-03-29 23:10:16 -0500

    Se agrega funcionalidad al boton de backup

---
+++ Frontend/WEB/js/backup.js
@@ -0,0 +1,37 @@
+function downloadBackup() {
+    var url = 'https://localhost:7084/api/Backup/backup/sql'; 
+
+    // aqui se realiza la solicitud GET al servidor para descargar el archivo SQL
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al descargar el archivo');
+        }
+        return response.blob(); // Convertir la respuesta a un blob
+    })
+    .then(blob => {
+        // Crear un objeto URL para el blob
+        var url = window.URL.createObjectURL(blob);
+
+        // Crear un enlace <a> para descargar el archivo
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = 'Copia_de_seguridad.sql'; // Nombre del archivo de respaldo
+        document.body.appendChild(a);
+        
+        // Hacer clic en el enlace para iniciar la descarga
+        a.click();
+
+        // Liberar el objeto URL
+        window.URL.revokeObjectURL(url);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error al descargar el archivo de respaldo');
+    });
+}
+
+// Llamar a la función para descargar el archivo cuando se haga clic en el botón
+document.getElementById('backupButton').addEventListener('click', function() {
+    downloadBackup();
+});
