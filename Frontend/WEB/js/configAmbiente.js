c552443396ac138b9b131877dfff1312746dd005
tatiana Hoyos <tatianavertel98@gmail.com>
2024-04-08 17:43:05 -0500

    se parametriza dominio de s3

--- Frontend/WEB/js/configAmbiente.js
+++ Frontend/WEB/js/configAmbiente.js
@@ -28,6 +28,7 @@ var modulo = {
 };
 
 let domainEC2 = "${DOMAIN-EC2}";
+let domainS3 = "${DOMAIN-S3}";
 var hostDomain = !domainEC2.includes("compute-1.amazonaws.com") ? "http://localhost:8081" : domainEC2;
-var hostDomainImage = !domainEC2.includes("compute-1.amazonaws.com") ? "http://localhost:8080" : domainEC2;
+var hostDomainImage = !domainS3.includes("s3.us-east-1.amazonaws.com") ? "http://localhost:8080" : domainS3;
 var hostDomainNotificaciones = !domainEC2.includes("compute-1.amazonaws.com") ? "https://localhost:7084" : domainEC2;
