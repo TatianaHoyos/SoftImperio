be0674258cd1c7bdc6e4ef7f6fe05a5cd64e6d8b
tatiana Hoyos <tatianavertel98@gmail.com>
2024-04-09 23:01:02 -0500

    se agrega una prueba unitaria

--- Frontend/Mobile/login/test/widget_test.dart
+++ Frontend/Mobile/login/test/widget_test.dart
@@ -9,27 +9,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:login/main.dart';
+import 'package:login/pages/inicio_page.dart';
+import 'package:login/pages/login_page.dart';
+import 'package:login/pages/pedido_productos_screen/cart_screen.dart';
+import 'package:login/pages/punto_de_venta_screen/punto_venta_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('MyApp Widget Test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-
-    // Verify that MyApp contains a MaterialApp widget.
+    
+    // Verifica que el widget MaterialApp esté presente
     expect(find.byType(MaterialApp), findsOneWidget);
-
-    // Verify that the MaterialApp widget contains a MultiProvider widget.
+    
+    // Verifica que el widget MultiProvider esté presente
     expect(find.byType(MultiProvider), findsOneWidget);
-
-    // Verify that the MultiProvider widget contains a MaterialApp widget.
-    expect(find.byType(MaterialApp), findsOneWidget);
-
-    // Verify that the MaterialApp widget has the correct routes defined.
-    //expect(find.byKey(const Key('/')), findsOneWidget);
-    expect(find.byKey(const Key('/punto_venta')), findsOneWidget);
-    expect(find.byKey(const Key('/screen_carrito')), findsOneWidget);
-    expect(find.byKey(const Key('/inicio')), findsOneWidget);
-
+    
+    // Verifica que el widget LoginPage esté presente
+    expect(find.byType(LoginPage), findsOneWidget);
+    
   });
 }
